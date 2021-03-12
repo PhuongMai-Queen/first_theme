2078e40578243c40deb7ea514fe3ac2420af0970
BinhIT <nbinh6538@gmail.com>
2021-03-12 21:19:03 +0700

    hellooooo

--- pages/carts/index.vue
+++ pages/carts/index.vue
@@ -5,9 +5,9 @@
         <h3>Giỏ hàng</h3>
       </div>
     </div>
-    <div class="ps-content pt-80 pb-80">
-      <div class="container ">
-        <div class="ps-cart-listing table-responsive">
+    <div class="ps-content pt-80 pb-80 ">
+      <div class="container table-responsive">
+        <div class="ps-cart-listing">
           <table class="table ps-cart__table">
             <thead>
             <tr>
@@ -70,94 +70,6 @@
             </tr>
             </tbody>
           </table>
-    <div class="ps-content pb-80">
-      <div class="ps-container">
-        <div class="ps-cart-listing">
-          <div class="row">
-            <div class="col-sm-9">
-              <table class="table ps-cart__table">
-                <tbody>
-                <tr>
-                  <td><a href="product-detail.html"><img class="mr-15" src="http://api.tvtp.vn/v0/img/uploads,5FEC524B4FCC6_1609323083.jpg"></a></td>
-                  <td><a class="ps-product__preview" href="product-detail.html">KUN sữa có đường 180ml thùng 48 hộp</a></td>
-                  <td>336,000đ</td>
-                  <td class="input-amount">
-                    <div class="form-group--number">
-                      <button class="minus"><span>-</span></button>
-                      <input class="form-control" type="text" value="2">
-                      <button class="plus"><span>+</span></button>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="ps-remove"></div>
-                  </td>
-                </tr>
-                <tr>
-                  <td><a href="product-detail.html"><img class="mr-15" src="http://api.tvtp.vn/v0/img/uploads,5FC5C57029F7A_1606796656.jpg"></a></td>
-                  <td><a class="ps-product__preview" href="product-detail.html">KUN sữa chua uống kem dâu túi 110ml thùng 24 túi</a></td>
-                  <td>116.000đ</td>
-                  <td>
-                    <div class="form-group--number">
-                      <button class="minus"><span>-</span></button>
-                      <input class="form-control" type="text" value="2">
-                      <button class="plus"><span>+</span></button>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="ps-remove"></div>
-                  </td>
-                </tr>
-                <tr>
-                  <td><a href="product-detail.html"><img class="mr-15" src="http://api.tvtp.vn/v0/img/uploads,5FEC5583E237B_1609323907.jpg"></a></td>
-                  <td><a class="ps-product__preview" href="product-detail.html">KUN sữa chua ăn vị dâu 75gr thùng 48 hộp</a></td>
-                  <td>282,000đ</td>
-                  <td>
-                    <div class="form-group--number">
-                      <button class="minus"><span>-</span></button>
-                      <input class="form-control" type="text" value="2">
-                      <button class="plus"><span>+</span></button>
-                    </div>
-                  </td>
-                  <td>
-                    <div class="ps-remove"></div>
-                  </td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="col-sm-3 sidenav">
-              <div class="ps-cart__total">
-           <div class="total">
-             <div>
-               <span class="note">Tạm tính : </span>
-             </div>
-             <div>
-             <span>2,548,000</span>vnđ
-           </div>
-           </div>
-                <div class="total">
-                  <div>
-                    <span class="note">Giảm giá : </span>
-                  </div>
-                  <div>
-                    <span>20</span>%
-                  </div>
-                </div>
-                <hr>
-                <div class="total">
-                  <div>
-                    <span class="note">Thành tiền : </span>
-                  </div>
-                  <div>
-                    <span class="danger">2,435,000 </span>vnđ
-                  </div>
-                </div>
-                <div class="mr-40">
-                  <button class="ps-btn ps-btn--fullwidth">Đặt Hàng<i class="fas fa-arrow-right"></i></button>
-                </div>
-              </div>
-            </div>
-          </div>
           <div class="ps-cart__actions">
             <div class="ps-cart__promotion">
               <div class="form-group">
